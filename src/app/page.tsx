d1ee91bc4b53e2f38772ba2706d5d4c07d1de3b7
Rama Lopez Cento <ramirolopezcento@icloud.com>
2024-05-12 22:27:37 +0200

    TrustLens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 lg:p-16">
       <div className="flex place-items-center flex-col max-w-lg my-16">
-        <h1 className="mb-3 text-3xl font-semibold">Welcome to Social</h1>
+        <h1 className="mb-3 text-3xl font-semibold">
+          Welcome to <span className="text-green-500">TrustLens</span>
+        </h1>
         <LensLogin />
       </div>
     </main>
