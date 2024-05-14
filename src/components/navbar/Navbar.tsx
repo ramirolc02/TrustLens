f4ee504e0695cc38f7234f0d34c0d73e58dbd6d8
Rama Lopez Cento <ramirolopezcento@icloud.com>
2024-05-14 19:03:19 +0200

    feat: refactor nabvar with usePathname() hook

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,68 +1,61 @@
 "use client"
 
+import { useActivePath } from "@/utils/navbar-helper"
 import {
   SessionType,
   useSession as useLensSession,
 } from "@lens-protocol/react-web"
 import Link from "next/link"
-import { useState } from "react"
+
+type NavigationItem = {
+  href: string
+  name: string
+}
+
+const restrictedNavigation: NavigationItem[] = [
+  { href: "/", name: "Home" },
+  { href: "/profiles", name: "Profiles" },
+  { href: "/publications", name: "Publications" },
+]
+const fullNavigation: NavigationItem[] = [
+  { href: "/", name: "Home" },
+  { href: "/profile", name: "Profile" },
+  { href: "/post", name: "Post" },
+  { href: "/profiles", name: "Profiles" },
+  { href: "/publications", name: "Publications" },
+]
 
 export default function Navbar() {
   const { data: session } = useLensSession()
+  const checkActivePath = useActivePath()
 
-  if (!session || !(session.type === SessionType.WithProfile)) {
-    return <NoLoginNavbar />
-  } else {
-    return <FullNavbar />
-  }
-}
-const NoLoginNavbar = () => {
-  const [activeLink, setActiveLink] = useState("/")
+  const navigation =
+    session && session.type === SessionType.WithProfile
+      ? fullNavigation
+      : restrictedNavigation
 
-  const handleLinkClick = (link: string) => {
-    setActiveLink(link)
-  }
   return (
     <>
       <div className="w-full h-20 bg-purple-500 sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
-            <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
-                <Link href="/">
-                  <p
-                    className={activeLink === "/" ? "font-bold text-lg" : ""}
-                    onClick={() => handleLinkClick("/")}
-                  >
-                    Home
-                  </p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/publications">
-                  <p
-                    className={
-                      activeLink === "/publications" ? "font-bold text-lg" : ""
-                    }
-                    onClick={() => handleLinkClick("/publications")}
-                  >
-                    Explore Publications
-                  </p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/profiles">
-                  <p
-                    className={
-                      activeLink === "/profiles" ? "font-bold text-lg" : ""
-                    }
-                    onClick={() => handleLinkClick("/profiles")}
-                  >
-                    Explore Profiles
-                  </p>
-                </Link>
-              </li>
-            </ul>
+            <nav>
+              <ul className="hidden md:flex gap-x-6 text-white">
+                {navigation.map(({ href, name }) => (
+                  <li key={href}>
+                    <Link href={href}>
+                      <span
+                        className={
+                          checkActivePath(href) ? "font-bold text-lg" : ""
+                        }
+                      >
+                        {name}
+                      </span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
@@ -70,81 +63,135 @@ const NoLoginNavbar = () => {
   )
 }
 
-const FullNavbar = () => {
-  const [activeLink, setActiveLink] = useState("/")
+// const NoLoginNavbar = () => {
+//   const [activeLink, setActiveLink] = useState("/")
 
-  const handleLinkClick = (link: string) => {
-    setActiveLink(link)
-  }
+//   const handleLinkClick = (link: string) => {
+//     setActiveLink(link)
+//   }
+//   return (
+//     <>
+//       <div className="w-full h-20 bg-purple-500 sticky top-0">
+//         <div className="container mx-auto px-4 h-full">
+//           <div className="flex justify-between items-center h-full">
+//             <ul className="hidden md:flex gap-x-6 text-white">
+//               <li>
+//                 <Link href="/">
+//                   <p
+//                     className={activeLink === "/" ? "font-bold text-lg" : ""}
+//                     onClick={() => handleLinkClick("/")}
+//                   >
+//                     Home
+//                   </p>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/publications">
+//                   <p
+//                     className={
+//                       activeLink === "/publications" ? "font-bold text-lg" : ""
+//                     }
+//                     onClick={() => handleLinkClick("/publications")}
+//                   >
+//                     Explore Publications
+//                   </p>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/profiles">
+//                   <p
+//                     className={
+//                       activeLink === "/profiles" ? "font-bold text-lg" : ""
+//                     }
+//                     onClick={() => handleLinkClick("/profiles")}
+//                   >
+//                     Explore Profiles
+//                   </p>
+//                 </Link>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
 
-  return (
-    <>
-      <div className="w-full h-20 bg-purple-500 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
-                <Link href="/">
-                  <p
-                    className={activeLink === "/" ? "font-bold text-lg" : ""}
-                    onClick={() => handleLinkClick("/")}
-                  >
-                    Home
-                  </p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/publications">
-                  <p
-                    className={
-                      activeLink === "/publications" ? "font-bold text-lg" : ""
-                    }
-                    onClick={() => handleLinkClick("/publications")}
-                  >
-                    Explore Publications
-                  </p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/profiles">
-                  <p
-                    className={
-                      activeLink === "/profiles" ? "font-bold text-lg" : ""
-                    }
-                    onClick={() => handleLinkClick("/profiles")}
-                  >
-                    Explore Profiles
-                  </p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/profile">
-                  <p
-                    className={
-                      activeLink === "/profile" ? "font-bold text-lg" : ""
-                    }
-                    onClick={() => handleLinkClick("/profile")}
-                  >
-                    My Profile
-                  </p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/post">
-                  <p
-                    className={
-                      activeLink === "/post" ? "font-bold text-lg" : ""
-                    }
-                    onClick={() => handleLinkClick("/post")}
-                  >
-                    Create Post
-                  </p>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
+// const FullNavbar = () => {
+//   const [activeLink, setActiveLink] = useState("/")
+
+//   const handleLinkClick = (link: string) => {
+//     setActiveLink(link)
+//   }
+
+//   return (
+//     <>
+//       <div className="w-full h-20 bg-purple-500 sticky top-0">
+//         <div className="container mx-auto px-4 h-full">
+//           <div className="flex justify-between items-center h-full">
+//             <ul className="hidden md:flex gap-x-6 text-white">
+//               <li>
+//                 <Link href="/">
+//                   <p
+//                     className={activeLink === "/" ? "font-bold text-lg" : ""}
+//                     onClick={() => handleLinkClick("/")}
+//                   >
+//                     Home
+//                   </p>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/publications">
+//                   <p
+//                     className={
+//                       activeLink === "/publications" ? "font-bold text-lg" : ""
+//                     }
+//                     onClick={() => handleLinkClick("/publications")}
+//                   >
+//                     Explore Publications
+//                   </p>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/profiles">
+//                   <p
+//                     className={
+//                       activeLink === "/profiles" ? "font-bold text-lg" : ""
+//                     }
+//                     onClick={() => handleLinkClick("/profiles")}
+//                   >
+//                     Explore Profiles
+//                   </p>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/profile">
+//                   <p
+//                     className={
+//                       activeLink === "/profile" ? "font-bold text-lg" : ""
+//                     }
+//                     onClick={() => handleLinkClick("/profile")}
+//                   >
+//                     My Profile
+//                   </p>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link href="/post">
+//                   <p
+//                     className={
+//                       activeLink === "/post" ? "font-bold text-lg" : ""
+//                     }
+//                     onClick={() => handleLinkClick("/post")}
+//                   >
+//                     Create Post
+//                   </p>
+//                 </Link>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
